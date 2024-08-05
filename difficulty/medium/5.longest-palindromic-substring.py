97705cb7e17ac9c515fdbe5dafb00f01d2bbb31c
devangtomar <devangtomar123@gmail.com>
2024-08-05 22:06:22 +0530

    Add solution for problem #5 med difficulty.. longest palindromic sub string

--- difficulty/medium/5.longest-palindromic-substring.py
+++ difficulty/medium/5.longest-palindromic-substring.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-# @lc code=end
+        if len(s) <= 1:
+            return s
 
+        Max_Len = 1
+        Max_Str = s[0]
+        for i in range(len(s) - 1):
+            for j in range(i + 1, len(s)):
+                if j - i + 1 > Max_Len and s[i : j + 1] == s[i : j + 1][::-1]:
+                    Max_Len = j - i + 1
+                    Max_Str = s[i : j + 1]
+
+        return Max_Str
+
+# @lc code=end
