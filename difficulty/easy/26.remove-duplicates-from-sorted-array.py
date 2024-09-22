0b93030674ad736175201fa35c9a94b0bd856fcb
devangtomar <devangtomar123@gmail.com>
2024-09-22 23:57:10 +0530

    Refactor mergeTwoLists function for clarity and efficiency

---
+++ difficulty/easy/26.remove-duplicates-from-sorted-array.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=26 lang=python3
+#
+# [26] Remove Duplicates from Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 0
+        if not nums:
+            return 0
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow + 1
+
+# @lc code=end
