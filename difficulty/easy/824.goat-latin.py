778446914a1e934c8c872687b9c3aecd3ebb133f
devangtomar <devangtomar123@gmail.com>
2024-10-11 19:16:41 +0530

    Add Solution class for Goat Latin problem

---
+++ difficulty/easy/824.goat-latin.py
@@ -0,0 +1,15 @@
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        vowels = set("aeiouAEIOU")
+        words = sentence.split()
+        result = []
+
+        for i, word in enumerate(words):
+            if word[0] in vowels:
+                goat_word = word + "ma"
+            else:
+                goat_word = word[1:] + word[0] + "ma"
+            goat_word += "a" * (i + 1)
+            result.append(goat_word)
+
+        return " ".join(result)
