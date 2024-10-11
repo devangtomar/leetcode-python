e1e63003e6cf233dbdc823137c2c0b2e9f247a74
devangtomar <devangtomar123@gmail.com>
2024-10-11 19:19:50 +0530

    Add Solution class for Integer to Roman problem

---
+++ difficulty/medium/12.integer-to-roman.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=12 lang=python3
+#
+# [12] Integer to Roman
+#
+
+# @lc code=start
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        hrns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        ths = ["", "M", "MM", "MMM"]
+
+        return ths[num // 1000] + hrns[(num % 1000) // 100] + tens[(num % 100) // 10] + ones[num % 10]
+
+# @lc code=end
+
