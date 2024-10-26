f26b4f8e1f1e84a20b1c05f25554cbd5bfaf655f
devangtomar <devangtomar123@gmail.com>
2024-10-26 22:03:51 +0530

    Add Solution class for Dungeon Game and Largest Rectangle in Histogram problems

---
+++ difficulty/easy/84.largest-rectangle-in-histogram.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=84 lang=python3
+#
+# [84] Largest Rectangle in Histogram
+#
+
+# @lc code=start
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        max_area = 0
+        heights = [0] + heights + [0]
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] > h:
+                height = heights[stack.pop()]
+                width = i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            stack.append(i)
+        return max_area
+# @lc code=end
+
